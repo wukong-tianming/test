52d860e10a6a0c44b6a17f546b34ce2d7ab920a4
wukong-tianming <19339140967@163.com>
2025-07-12 09:38:00 +0000

    fix

--- read.py
+++ read.py
@@ -1,9 +1,9 @@
 ONLINE_DOWNLOAD_SOURCES = [
     # 直接下载ZIP格式（优先）
     "https://furcate.eu/FILES/{appid}.zip",
-    "https://assiw.cngames.site/qindan/{app_id}.zip",
+    "https://assiw.cngames.site/qindan/{appid}.zip",
     "https://steamdatabase.s3.eu-north-1.amazonaws.com/{appid}.zip",
-    "https://cysaw.top/uploads/{app_id}.zip",
+    "https://cysaw.top/uploads/{appid}.zip",
 
     # GitHub仓库路径（使用CDN代理）
     {"githubcangku": {
